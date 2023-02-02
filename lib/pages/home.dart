31ad252e8345a75ceebd8aa220471946730cc5f0
Cyber-Lord <Umaralfaruq02@gmail.com>
2023-02-02 10:10:37 +0100

    Added a top container + safearea

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -46,6 +46,68 @@ class _HomeState extends State<Home> {
           preferredSize: Size.fromHeight(75.0),
         ),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const ContainerWithBoxDecorationWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerWithBoxDecorationWidget extends StatelessWidget {
+  const ContainerWithBoxDecorationWidget({Key, key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100.0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(100.0),
+                bottomRight: Radius.circular(10.0),
+              ),
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Colors.lightGreen.shade500]),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(
+                    0.0,
+                    10.0,
+                  ),
+                  blurRadius: 10.0,
+                  color: Colors.grey,
+                ),
+              ]),
+          child: Center(
+            child: Text(
+              "Container",
+              style: TextStyle(
+                color: Colors.purpleAccent,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.deepPurple,
+                decorationStyle: TextDecorationStyle.dotted,
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 4,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        )
+      ],
     );
   }
 }

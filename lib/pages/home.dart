e829fde5fb3a5f89437f921cecdc824c2192cf9c
Cyber-Lord <Umaralfaruq02@gmail.com>
2023-02-03 17:33:40 +0100

    added images

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class TodoMenuItem {
+  final String title;
+  final Icon icon;
+
+  TodoMenuItem({required this.title, required this.icon});
+
+  List<TodoMenuItem> foodList = [
+    TodoMenuItem(title: "Fast Food", icon: Icon(Icons.fastfood_rounded)),
+    TodoMenuItem(title: "Remind Me", icon: Icon(Icons.alarm_add)),
+    TodoMenuItem(title: "Flight", icon: Icon(Icons.flight)),
+    TodoMenuItem(title: "Music", icon: Icon(Icons.audiotrack)),
+  ];
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -65,6 +79,12 @@ class _HomeState extends State<Home> {
                 const RowWidget(),
                 Divider(),
                 const ColumnAndRowWidgetNesting(),
+                Image(
+                  image: AssetImage("lib/assets/images/ratio.jpg"),
+                  fit: BoxFit.cover,
+                )
+                //   Image.network(
+                //       "https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/TunnelBear_screenshot.png/220px-TunnelBear_screenshot.png")
               ],
             ),
           ),
@@ -205,3 +225,38 @@ class ColumnAndRowWidgetNesting extends StatelessWidget {
     );
   }
 }
+
+// class PopupMenuButtonWidget extends StatelessWidget
+//     implements PreferredSizeWidget {
+//   const PopupMenuButtonWidget({Key, key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.lightGreen.shade100,
+//       height: preferredSize.height,
+//       width: double.infinity,
+//       child: Center(
+//         child: PopupMenuButton<TodoMenuItem>(
+//           icon: Icon(Icons.view_list),
+//           onSelected: ((valueSelected) {
+//             print("Value Selected : ${valueSelected.title}");
+//           }),
+//           itemBuilder: (BuildContext context) {
+//             return foodList.map((TodoMenuItem todoMenuItem) {
+//               return PopupMenuItem<TodoMenuItem>(
+//                 value: todoMenuItem,
+//                 child: Row(
+//                   children: [
+//                     Icon(todoMenuItem.icon.icon),
+//                     Padding(padding: EdgeInsets.all(16.0)),
+//                     Text(todoMenuItem.title)
+//                   ],
+//                 ),
+//               );
+//             }).toList();
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
